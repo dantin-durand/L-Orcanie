bed06ee5aa854365f75dfe80b64c8cbec05de575
dantin-durand <durand.dantin.pro@gmail.com>
2025-05-31 14:32:39 +0200

    Ajustement de la largeur maximale du conteneur du menu, réorganisation de la structure des cartes pour les plats et ajout d'une section de détails pour les éléments sélectionnés.

--- src/features/menu/menu.page.tsx
+++ src/features/menu/menu.page.tsx
@@ -50,7 +50,7 @@ export default function MenuPage() {
 
   return (
     <div className="bg-gray-100 px-[5%] py-10 min-h-screen w-full">
-      <div className="max-w-3xl mx-auto">
+      <div className="max-w-[1400px] mx-auto">
         <div className="flex items-center space-x-2 mb-6 justify-end">
           <Label htmlFor="realname-mode">Afficher les noms</Label>
           <Switch
@@ -68,87 +68,89 @@ export default function MenuPage() {
             Réinitialiser les quantités
           </Button>
         )}
-        {menu.map((item: MenuItem, itemIndex: number) => (
-          <Card key={itemIndex} className="mb-4 gap-0 px-4">
-            <div className="sticky top-0 bg-white z-10 px-2 py-3 border-b border-gray-200 mb-6">
-              <Heading1 className="">{item.title}</Heading1>
-            </div>
-            {item.dishes.map((dish, dishIndex) => {
-              const id = getDishId(itemIndex, dishIndex);
-              const qty = quantities[id] || 0;
-              const totalDishPrice = dish.price * qty;
-
-              return (
-                <CardHeader
-                  key={dishIndex}
-                  className={`px-2 py-3 rounded-lg border ${
-                    dish.pinned
-                      ? "border-yellow-500 bg-yellow-50 px-3"
-                      : " border-transparent"
-                  } ${qty > 0 ? "bg-green-50" : ""}`}
-                >
-                  <CardTitle>
-                    {showRealName ? dish.realName : dish.label}
-                  </CardTitle>
-                  <CardDescription>
-                    {showRealName ? dish.label : dish.realName}
-                  </CardDescription>
-                  <CardAction className="flex flex-col items-end gap-2">
-                    <p>
-                      {qty > 1
-                        ? `$${totalDishPrice.toFixed(
-                            2
-                          )} (${qty} x $${dish.price.toFixed(2)})`
-                        : `$${dish.price.toFixed(2)}`}
-                    </p>
-                    <select
-                      className="ml-2 border rounded p-1"
-                      value={qty}
-                      onChange={(e) =>
-                        setQuantities((prev) => ({
-                          ...prev,
-                          [id]: Number(e.target.value),
-                        }))
-                      }
-                    >
-                      {Array.from({ length: 41 }, (_, i) => i).map((num) => (
-                        <option key={num} value={num}>
-                          {num}
-                        </option>
-                      ))}
-                    </select>
-                  </CardAction>
-                </CardHeader>
-              );
-            })}
-          </Card>
-        ))}
-
-        {selectedItems.length > 0 && (
-          <Card className="mt-4">
-            <CardHeader>
-              <CardTitle>Détails</CardTitle>
-              <div className="mt-2 text-sm text-gray-700">
-                {selectedItems.map((item, index) => (
-                  <div
-                    key={index}
-                    className="flex justify-between border-b border-dashed py-1"
-                  >
-                    <span>{item.label}</span>
-                    <span>
-                      {item.qty} × ${item.price.toFixed(2)} = $
-                      {(item.qty * item.price).toFixed(2)}
-                    </span>
-                  </div>
-                ))}
+        <div className="w-full grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {menu.map((item: MenuItem, itemIndex: number) => (
+            <Card key={itemIndex} className="mb-4 gap-0 px-4 self-start">
+              <div className="sticky top-0 bg-white z-10 px-2 py-3 border-b border-gray-200 mb-6">
+                <Heading1 className="">{item.title}</Heading1>
               </div>
-            </CardHeader>
-            <CardHeader>
-              <CardTitle className="mt-1">TOTAL</CardTitle>
-              <CardAction>${total.toFixed(2)}</CardAction>
-            </CardHeader>
-          </Card>
-        )}
+              {item.dishes.map((dish, dishIndex) => {
+                const id = getDishId(itemIndex, dishIndex);
+                const qty = quantities[id] || 0;
+                const totalDishPrice = dish.price * qty;
+
+                return (
+                  <CardHeader
+                    key={dishIndex}
+                    className={`px-2 py-3 rounded-lg border ${
+                      dish.pinned
+                        ? "border-yellow-500 bg-yellow-50 px-3"
+                        : " border-transparent"
+                    } ${qty > 0 ? "bg-green-50" : ""}`}
+                  >
+                    <CardTitle>
+                      {showRealName ? dish.realName : dish.label}
+                    </CardTitle>
+                    <CardDescription>
+                      {showRealName ? dish.label : dish.realName}
+                    </CardDescription>
+                    <CardAction className="flex flex-col items-end gap-2">
+                      <p>
+                        {qty > 1
+                          ? `$${totalDishPrice.toFixed(
+                              2
+                            )} (${qty} x $${dish.price.toFixed(2)})`
+                          : `$${dish.price.toFixed(2)}`}
+                      </p>
+                      <select
+                        className="ml-2 border rounded p-1"
+                        value={qty}
+                        onChange={(e) =>
+                          setQuantities((prev) => ({
+                            ...prev,
+                            [id]: Number(e.target.value),
+                          }))
+                        }
+                      >
+                        {Array.from({ length: 41 }, (_, i) => i).map((num) => (
+                          <option key={num} value={num}>
+                            {num}
+                          </option>
+                        ))}
+                      </select>
+                    </CardAction>
+                  </CardHeader>
+                );
+              })}
+            </Card>
+          ))}
+
+          {selectedItems.length > 0 && (
+            <Card className="mt-4 md:mt-0">
+              <CardHeader>
+                <CardTitle>Détails</CardTitle>
+                <div className="mt-2 text-sm text-gray-700">
+                  {selectedItems.map((item, index) => (
+                    <div
+                      key={index}
+                      className="flex justify-between border-b border-dashed py-1"
+                    >
+                      <span>{item.label}</span>
+                      <span>
+                        {item.qty} × ${item.price.toFixed(2)} = $
+                        {(item.qty * item.price).toFixed(2)}
+                      </span>
+                    </div>
+                  ))}
+                </div>
+              </CardHeader>
+              <CardHeader>
+                <CardTitle className="mt-1">TOTAL</CardTitle>
+                <CardAction>${total.toFixed(2)}</CardAction>
+              </CardHeader>
+            </Card>
+          )}
+        </div>
         {selectedItems.length > 0 && (
           <Button
             className="w-full mt-6"
